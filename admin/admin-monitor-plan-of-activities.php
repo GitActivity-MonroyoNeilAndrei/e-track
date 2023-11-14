d2f3f77248dd842113cda4fe076a827b7877d26c
NeilAndrei <andreimonroyo0@gmail.com>
2023-11-14 11:28:06 +0800

    nav-links - being responsive fixed

--- admin/admin-monitor-plan-of-activities.php
+++ admin/admin-monitor-plan-of-activities.php
@@ -107,21 +107,57 @@ $nameOfAct = returnNameOfAct();
       </div>
     </div>
     <div class="page-content">
-      <div class="nav-links">
+    <div class="nav-links">
         <nav style="position: sticky; top: 4vh;">
           <ul>
-          <li onclick="window.location.href='admin-homepage.php'">Dashboard</li>
-            <li onclick="window.location.href='admin-list-of-users.php'" class="mb-4 border-bottom border-dark">List of Users</li>
-            <li onclick="window.location.href='admin-election.php'">Election</li>
-            <li onclick="window.location.href='admin-monitor-election-result.php'" class="mb-4 border-bottom border-dark">Monitor Election Result </li>
-            <li onclick="window.location.href='admin-student-organization.php'" class="mb-4 border-bottom border-dark">Student Organization</li>
-            <li onclick="window.location.href='admin-plan-of-activities.php'">Plan of Activities</li>
-            <li onclick="window.location.href='admin-list-of-plan-of-activities.php'">List of Plan of Activities</li>
-            <li onclick="window.location.href='admin-monitor-plan-of-activities.php'" class="mb-4 border-bottom border-dark bg-dark-gray2">Monitor Plan of Activities</li>
-            <li onclick="window.location.href='admin-accomplishment-report.php'">Accomplishment Report</li>
-            <li onclick="window.location.href='admin-list-of-accomplishment-report.php'" class="mb-4 border-bottom border-dark">List of Accomplishment Report</li>
-            <li onclick="window.location.href='admin-evaluation-of-activities.php'">Evaluation of Activities</li>
-            <li onclick="window.location.href='admin-report-to-ovpsas.php'">Report to OVPSAS</li>
+            <li onclick="window.location.href='admin-homepage.php'">
+              <span>Dashboard</span>
+              <span><i class="fa-solid fa-bars"></i></span>
+            </li>
+            <li onclick="window.location.href='admin-list-of-users.php'" class="mb-4 border-bottom border-dark">
+              <span>List of Users</span>
+              <span><i class="fa-solid fa-users"></i></span>
+            </li>
+            <li onclick="window.location.href='admin-election.php'">
+              <span>Election</span>
+              <span><i class="fa-solid fa-envelope-open-text"></i></span>
+            </li>
+            <li onclick="window.location.href='admin-monitor-election-result.php'" class="mb-4 border-bottom border-dark">
+              <span>Monitor Election Result</span>
+              <span><i class="fa-solid fa-square-poll-horizontal"></i></span>
+            </li>
+            <li onclick="window.location.href='admin-student-organization.php'" class="mb-4 border-bottom border-dark">
+              <span>Student Organization</span>
+              <span><i class="fa-solid fa-sitemap"></i></span>
+            </li>
+            <li onclick="window.location.href='admin-plan-of-activities.php'">
+              <span>Plan of Activities</span>
+              <span><i class="fa-solid fa-check-to-slot"></i></span>
+            </li>
+            <li onclick="window.location.href='admin-list-of-plan-of-activities.php'">
+              <span>List of Plan of Activities</span>
+              <span><i class="fa-solid fa-list-ul"></i></span>
+            </li>
+            <li onclick="window.location.href='admin-monitor-plan-of-activities.php'" class="mb-4 border-bottom border-dark bg-dark-gray2">
+              <span>Monitor Plan of Activities</span>
+              <span><i class="fa-solid fa-tv"></i></span>
+            </li>
+            <li onclick="window.location.href='admin-accomplishment-report.php'">
+              <span>Accomplishment Report</span>
+              <span><i class="fa-solid fa-check-to-slot"></i></span>
+            </li>
+            <li onclick="window.location.href='admin-list-of-accomplishment-report.php'" class="mb-4 border-bottom border-dark">
+              <span>List of Accomplishment Report</span>
+              <span><i class="fa-solid fa-list-ul"></i></span>
+            </li>
+            <li onclick="window.location.href='admin-evaluation-of-activities.php'">
+              <span>Evaluation of Activities</span>
+              <span><i class="fa-solid fa-clipboard-check"></i></span>
+            </li>
+            <li onclick="window.location.href='admin-report-to-ovpsas.php'">
+              <span>Report to OVPSAS</span>
+              <span><i class="fa-solid fa-envelope-circle-check"></i></span>
+            </li>
           </ul>
         </nav>
       </div>
--- student-org/add-plan-of-activity.php
+++ student-org/add-plan-of-activity.php
@@ -80,13 +80,29 @@ if(isset($_POST['submit'])) {
       </div>
     </div>
     <div class="page-content">
-      <div class="nav-links">
+    <div class="nav-links">
         <nav style="position: sticky; top: 4vh;">
           <ul>
-            <li onclick="window.location.href='student-org-homepage.php'">Dashboard</li>
-            <li onclick="window.location.href='student-org-monitor-election.php'" class="mb-4 border-bottom border-dark">Monitor Election Result </li>
-            <li onclick="window.location.href='student-org-plan-of-activities.php'" class="bg-dark-gray2">Plan of Activities</li>
-            <li onclick="window.location.href='student-org-accomplishment-report.php'" class="mb-4 border-bottom border-dark">Accomplishment Report</li>
+            <li onclick="window.location.href='student-org-homepage.php'">
+              <span>Dashboard</span>
+              <span><i class="fa-solid fa-bars"></i></span>
+            </li>
+            <li onclick="window.location.href='student-org-monitor-election.php'" class="mb-4 border-bottom border-dark">
+              <span>Monitor Election Result</span>
+              <span><i class="fa-solid fa-square-poll-horizontal"></i></span> 
+            </li>
+            <li onclick="window.location.href='student-org-plan-of-activities.php'" class="bg-dark-gray2">
+              <span>Plan of Activities</span>
+              <span><i class="fa-solid fa-check-to-slot"></i></span>
+            </li>
+            <li onclick="window.location.href='student-org-monitor-activities.php'">
+              <span>Monitor Plan of Activities</span>
+              <span><i class="fa-solid fa-tv"></i></span>
+            </li>
+            <li onclick="window.location.href='student-org-accomplishment-report.php'" class="mb-4 border-bottom border-dark">
+              <span>Accomplishment Report</span>
+              <span><i class="fa-solid fa-check-to-slot"></i></span>
+            </li>
           </ul>
         </nav>
       </div>
